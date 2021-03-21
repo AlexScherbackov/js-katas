9cd553bbcb5e04207fdfd3570c651a0ec4609285
Alexandr <jsprog2017@yandex.ru>
2021-03-21 15:04:48 +0300

    add second version of checkrackets

--- src/checkBrackets.js
+++ src/checkBrackets.js
@@ -1,4 +1,4 @@
-const checkBrackets = (str, config) => {
+const checkBrackets1 = (str, config) => {
   const mappedConfig = new Map(config);
   const arrOfStr = str.split('');
   const configEntries = Object.fromEntries(mappedConfig);
@@ -12,13 +12,36 @@ const checkBrackets = (str, config) => {
     (openBracket) => {
       const closeBracket = mappedConfig.get(openBracket);
       const openQuantity = calcQuantity(openBracket, arrOfStr);
-      const closeQuantity = calcQuantity(closeBracket, arrOfStr); 
+      const closeQuantity = calcQuantity(closeBracket, arrOfStr);
       if (openBracket === closeBracket && openQuantity % 2 !== 0) {
         return false;
       }
       return openQuantity === closeQuantity;
-    }
+    },
   );
+};
+
+const checkBrackets = (str, config) => {
+  const mappedConfig = new Map(config);
+  const arrOfStr = str.split('');
+  const stackOfBrackets = arrOfStr.reduce(
+    (acc, i) => {
+      if (mappedConfig.has(i) && mappedConfig.get(i) !== i) {
+        return [...acc, i];
+      }
+
+      const lastBracket = acc[acc.length - 1];
+
+      if (mappedConfig.get(lastBracket) === i) {
+        const newAcc = acc.slice(0, acc.length - 1);
+        return newAcc;
+      }
+      return [...acc, i];
+    },
+    [],
+  );
+
+  return stackOfBrackets.length === 0;
 };
 
 export default checkBrackets;
