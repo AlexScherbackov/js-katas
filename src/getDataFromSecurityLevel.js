bf689418082581b06f4b4b5363f572a928190696
Alexandr <jsprog2017@yandex.ru>
2021-03-26 14:43:37 +0300

    refactor getDataFromSecurityLevel

--- src/getDataFromSecurityLevel.js
+++ src/getDataFromSecurityLevel.js
@@ -1,8 +1,8 @@
 const getData = (arr, level) => {
   if (level === 0) {
-    return arr.filter((item) => typeof item !== 'object');
+    return arr.filter((item) => !Array.isArray(item));
   }
-  const nextLevel = arr.filter((item) => typeof item === 'object').flat();
+  const nextLevel = arr.filter((item) => Array.isArray(item)).flat();
   return getData(nextLevel, level - 1);
 };
 
