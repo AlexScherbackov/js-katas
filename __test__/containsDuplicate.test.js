59702a607ce420bb5aa5f12803f7a43411f7475d
Щербаков Александр Владимирович <sherbakov@arcsinus.ru>
2025-02-15 11:40:15 +0300

    add testcase with different types for containsDuplicate

--- __test__/containsDuplicate.test.js
+++ __test__/containsDuplicate.test.js
@@ -1,7 +1,7 @@
 import containsDuplicate from '../src/containsDuplicate';
 
 const arrayWithDuplicate = [1, 2, 3, 1];
-const arrayWithDuplicate1 = [1, 2, 3, '1', {}, ['a', 'c']];
+const arrayWithDuplicate1 = [1, 2, 3, '1', {}, ['a', 'c'], {}, []];
 const arrayWhithoutDuplicate = [1, 2, 3, 4];
 
 describe('Function should take a array. If array has duplicate elements function should return true else false', () => {
